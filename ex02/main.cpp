05bb08049e3dfeb00bd858f4f8e3b3fa78734257
nefimov <n.a.efimov@gmail.com>
2025-12-05 12:08:56 +0100

    update tests in main.cpp

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -23,6 +23,25 @@ int main(void) {
 	ft.takeDamage(90);
 	ft.takeDamage(2);
 
+	std::cout << std::endl << "==== FragTraps array ====" << std::endl;
+	std::cout << "<Constructors>" << std::endl;
+	FragTrap *ftraps[3];
+	for (int i = 0; i < 3; ++i) {
+		ftraps[i] = new FragTrap("one_FragTrap");
+	}
+	std::cout << "<Constructors done>" << std::endl;
+
+	std::cout << std::endl << "<Methods>" << std::endl;
+	for (int i = 0; i < 3; ++i) {
+		ftraps[i]->attack("Phill");
+	}
+	std::cout << "<Methods done>" << std::endl;
+
+	std::cout << std::endl << "<Destructors>" << std::endl;
+	for (int i = 0; i < 3; ++i) {
+		delete(ftraps[i]);
+	}
+	std::cout << "<Destructors done>" << std::endl << std::endl;
 
 	return 0;
 }
